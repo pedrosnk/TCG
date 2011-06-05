371312f48a497a650ce6e23656002ef780b9b490
Volmer Soares <lrvolmer@gmail.com>
2011-06-05 01:18:33 -0300

    my last job

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,9 @@ Tcg::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'test@gmail.com',
-    :password             => '',
+    :user_name            => 'railstest0@gmail.com',
+    :domain               => 'gmail.com',
+    :password             => 'ihatemyjob',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 
--- db/migrate/20110529035907_cards_users.rb
+++ db/migrate/20110529035907_cards_users.rb
@@ -1,8 +1,8 @@
-#class CardsUsers < ActiveRecord::Migration
-#  def change
-#    create_table :cards_users, :id=>false do |t|
-#      t.references :card, :null => false
-#      t.references :user, :null => false
-#    end
-#  end
-#end
+class CardsUsers < ActiveRecord::Migration
+  def change
+    create_table :cards_users, :id=>false do |t|
+      t.references :card, :null => false
+      t.references :user, :null => false
+    end
+  end
+end
